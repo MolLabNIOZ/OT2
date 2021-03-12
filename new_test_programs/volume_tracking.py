276e16ce3d5ea68eb9c4d0215296c3cdd8262cb2
29sv8 <73705620+29sv8@users.noreply.github.com>
2021-03-12 16:31:45 +0100

    Update volume_tracking.py
    Added set rail lights.
    Volume tracking works but needs to be perfected.
    Added function that when the mastermix is running out the pipette homes and pauses (still needs to be tested). Added some comments to variables.

--- new_test_programs/volume_tracking.py
+++ new_test_programs/volume_tracking.py
@@ -9,11 +9,6 @@ import json
 #import math for tracking calculations
 import math
 
-#open json file of 5mL tube rack and set as variable
-with open("C:/Users/svreugdenhil/Documents/GitHub/OT2/" 
-          "custom_labware_definitions/eppendorf_15_tuberack_5000ul/"
-          "eppendorf_15_tuberack_5000ul.json") as labware_file:
-    labware_def_5mL = json.load(labware_file)
 
 # Metadata
 metadata = {
@@ -35,10 +30,22 @@ def run(protocol: protocol_api.ProtocolContext):
         'biorad_96_wellplate_200ul_pcr', 
         3, 
         '96well_plate')
-    tubes_5mL = protocol.load_labware_from_definition(
-        labware_def_5mL, #variable derived from opening json
+    #for robot
+    tubes_5mL = protocol.load_labware(
+        'eppendorf_15_tuberack_5000ul', 
         2, 
         '5mL_tubes')
+    
+    # #for simulator
+    # #open json file of 5mL tube rack and set as variable
+    # with open("C:/Users/svreugdenhil/Documents/GitHub/OT2/" 
+    #       "custom_labware_definitions/eppendorf_15_tuberack_5000ul/"
+    #       "eppendorf_15_tuberack_5000ul.json") as labware_file:
+    #     labware_def_5mL = json.load(labware_file)
+    # tubes_5mL = protocol.load_labware_from_definition( 
+    #     labware_def_5mL, #variable derived from opening json
+    #     2, 
+    #     '5mL_tubes')
     
     # Loading pipettes
     p300 = protocol.load_instrument(
@@ -47,17 +54,33 @@ def run(protocol: protocol_api.ProtocolContext):
     # Set variables for volume_tracking
     start_vol = 3000 #starting volume at the beginning of the protocol in ul
     diameter = 13.3 #diameter of the top of the tube in mm
-    start_height = start_vol/(math.pi*((diameter/2)**2))
-    transfer_vol = 200
-    delta_height =  transfer_vol/(math.pi*((diameter/2)**2))
-    current_height = start_height - 1
+    start_height = start_vol/(math.pi*((diameter/2)**2)) #height of the starting volume
+    transfer_vol = 200 #volume that is transfered each time
+    delta_height =  transfer_vol/(math.pi*((diameter/2)**2)) # height of the transferred volume
+    current_height = start_height - 1 #height - something to make sure pipette is submerged
     
+    #set lights on
+    protocol.set_rail_lights(True)
+    
+    p300.pick_up_tip(tips_200['A3'])
     for well in plate_96.wells():
-        p300.transfer(transfer_vol, tubes_5mL['A1'], well.bottom(current_height))
-        if current_height - delta_height > 1:
-            current_height = current_height - delta_height
+        p300.transfer(
+            transfer_vol, 
+            tubes_5mL['A1'].bottom(current_height), 
+            well,
+            new_tip='never',
+            blow_out='true',
+            blowout_location='source well'
+            )
+        if current_height - delta_height >= 1: #make sure bottom is never reached
+            current_height = current_height - delta_height #calculate new hight after pipetting step
+        else: 
+            protocol.home()
+            protocol.pause('Your mix is finished.')
+    p300.drop_tip()
     
-    
+    #set lights off
+    protocol.set_rail_lights(False)    
     
     
     

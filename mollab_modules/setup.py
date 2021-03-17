4de4389c13490d22874cbd8b61b633ea030d239e
29sv8 <73705620+29sv8@users.noreply.github.com>
2021-03-17 11:29:17 +0100

    Update setup.py
    Created setup.py for python package

--- mollab_modules/setup.py
+++ mollab_modules/setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(name='mollab_modules',
+      version='0.1',
+      description='Modules for OT2',
+      url='https://github.com/29sv8/OT2/tree/main/mollab_modules',
+      author='MB&SV',
+      author_email='mollab@nioz.nl',
+      license='',
+      packages=['mollab_modules'],
+      zip_safe=False)

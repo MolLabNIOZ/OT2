4ae22c42955c359c3ef659cfabc2a454b6d3710a
29sv8 <73705620+29sv8@users.noreply.github.com>
2021-06-10 15:26:42 +0200

    Made protocol for eDNA test PCR fish adding samples
    adding samples with eve

--- mollab_protocols/simulate_protocol.py
+++ mollab_protocols/simulate_protocol.py
@@ -4,7 +4,7 @@
 from opentrons.simulate import simulate, format_runlog
 
 # Read file
-protocol_file = open("C:/Users/svreugdenhil/Documents/GitHub/OT2/mollab_protocols/project_related/210608_eDNA_fish_test_PCR_WALL-E.py")
+protocol_file = open("C:/Users/svreugdenhil/Documents/GitHub/OT2/mollab_protocols/project_related/210608_eDNA_fish_test_PCR_EVE.py")
 
 # Simulate protocol
 runlog, _bundle = simulate(protocol_file)

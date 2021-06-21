d087399961c3056e55d4384734d2086a6ae71eb8
Maartje <brouwer.mm@gmail.com>
2021-06-21 10:32:04 +0200

    finished qPCR1 for Dina
    First aliquoting dilution water, then aliquoting mastermix.
    Then  take every sample, dilute it in the water and then transfer the dilution to the PCR.
    Then take the sample_mix, dilute it. Transfer the dilution to an entire column.

--- mollab_protocols/project_related/Dina/210622-515F_806RB_qPCR1_DINA_EVE.py
+++ mollab_protocols/project_related/Dina/210622-515F_806RB_qPCR1_DINA_EVE.py
@@ -1,4 +1,4 @@
-well# =============================================================================
+# =============================================================================
 # Author(s): Maartje Brouwer
 # Creation date: 210616
 # Description: qPCR protocol. First qPCR of a batch, including 3x std curve
@@ -24,19 +24,22 @@ from mollab_modules import volume_tracking_v1 as vt
 metadata = {
     'protocolName': '210622-515F_806RB_qPCR1_DINA_EVE',
     'author': 'MB <maartje.brouwer@nioz.nl>',
-    'description': ('16S qPCR - aliquoting mix and primers,'
-                    'then diluting + adding sample'),
+    'description': ('16S qPCR - aliquoting water and mix,'
+                    'then diluting + adding samples. std dil. series by hand'),
     'apiLevel': '2.9'}
 
 def run(protocol: protocol_api.ProtocolContext):
     """
-    First qPCR of a batch
-    Aliquoting Phusion PCRmix with EvaGreen added from a 5 mL tube to a 
-    96-wells plate; using volume tracking so that the pipette starts 
-    aspirating at the starting height of the liquid and goes down as the 
-    volume decreases.
+    First qPCR of a batch.
+    First aliquot water for diluting samples into a 96_wells plate. 
+    The source = 3 5mL tubes. Use volume tracking, which resets after every
+    tube. 
+    Then aliquoting Phusion PCRmix with EvaGreen added from a 5 mL tube to a 
+    96-wells plate; use volume tracking.
     After that, dilute samples 100x and add to PCR mix.
-    Also include a standard sample mix and 3x standard curve
+    Also include a standard sample mix (dilute one time but distribute to an
+    entire column.
+    3 x dilution series, need to be added by hand.
     """
 # =============================================================================
 
@@ -144,22 +147,35 @@ def run(protocol: protocol_api.ProtocolContext):
     mastermix = tubes_5mL['C1']
     H2O = ([tubes_5mL.wells_by_name()[well_name] for well_name in
          ['B1', 'B2', 'B3']])
-    samples = (                                                           
-        ([sample_strips_1.columns_by_name()[collumn_name] 
-          for collumn_name in ['1','3', '5', '7', '9', '11']]) 
+    
+    samples = []
+    sample_columns = (                                                           
+        ([sample_strips_1.columns_by_name()[column_name] 
+          for column_name in ['1','3', '5', '7', '9', '11']]) 
         + 
-        ([sample_strips_2.columns_by_name()[collumn_name] 
-          for collumn_name in ['1','3']]) 
+        ([sample_strips_2.columns_by_name()[column_name] 
+          for column_name in ['1','3']]))
         # for 88 samples to 9, for 64 samples to 3
-        )
-    sample_mix = sample_strips_2['A11']
-    sample_dest = (
-        [plate_96_qPCR.collumns_by_name()[collumn_name] for collumn_name in
+    for column in sample_columns:
+        for well in column:
+            samples.append(well)
+        
+    sample_dest = []
+    sample_dest_columns = (
+        [plate_96_qPCR.columns_by_name()[column_name] for column_name in
          ['5','6', '7', '8', '9', '10', '11', '12']
         ])
-        # For PCR with std dilution series, start at 5, otherwise at 2
-    sample_mix_dest = plate_96_qPCR.collumns_by_name()['4']
-      ## Placement of different tubes on deck
+      # For PCR with std dilution series, start at 5, otherwise at 2
+    for column in sample_dest_columns:
+        for well in column:
+            sample_dest.append(well)
+        
+    
+    sample_mix = sample_strips_2['A11']
+    sample_mix_dest = []
+    for well in plate_96_qPCR.columns_by_name()['4']:
+        sample_mix_dest.append(well)
+      
 # =============================================================================
 
 
@@ -191,7 +207,6 @@ def run(protocol: protocol_api.ProtocolContext):
             container = container_water
             dispension_vol = dispension_vol_water
             
-          ## current height of mix in the mix tube is calculated start_height_mix
         for i, well in enumerate(destination):
           ## aliquot mix in entire qPCR plate, for each well do the following:  
            
@@ -200,16 +215,16 @@ def run(protocol: protocol_api.ProtocolContext):
             
             if i == 0: 
                 p300.pick_up_tip()
-                  ## If we are at the first well, start by picking up a tip.    ##
+                  ## If we are at the first well, start by picking up a tip.##
             elif i % 8 == 0:
                 p300.drop_tip()
                 p300.pick_up_tip()
-                  ## Then, after every 8th well, drop tip and pick up a new one.##
+                  ## Then, after every 8th well, drop tip and pick up new   ##
             
             current_height, pip_height, bottom_reached = vt.volume_tracking(
                 container, dispension_vol, current_height)
-                  ## call volume_tracking function, obtain current_height,      ##
-                  ## pip_height and whether bottom_reached.                     ##
+                  ## call volume_tracking function, obtain current_height,  ##
+                  ## pip_height and whether bottom_reached.                 ##
             
             if bottom_reached:
                 if aliquot == 'dilution_plate':
@@ -225,40 +240,107 @@ def run(protocol: protocol_api.ProtocolContext):
                     
                 elif aliquot == 'PCR_plate':    
                     aspiration_location = source.bottom(z=1)
-                    protocol.comment("You've reached the bottom of the MM-tube!")
+                    protocol.comment("You've reached the bottom of the tube!")
             
             else:
                 aspiration_location = source.bottom(pip_height)
-                  ## Set the location of where to aspirate from.                ##
-                  ## Because we put this in the loop, the location will change  ##
-                  ## to the newly calculated height after each pipetting step.  ##  
-                  ## To prevent the pipette from crashing into the bottom, we   ##
-                  ## tell it to keep pipetting from right above the bottom.     ##
+                  ## Set the location of where to aspirate from.            ##
     
             #### The actual aliquoting of mastermix
             p300.aspirate(aspiration_vol, aspiration_location)
-              ## Aspirate the amount specified in aspiration_vol from the       ##
-              ## location specified in aspiration_location.                     ##
+              ## Aspirate the amount specified in aspiration_vol from the   ##
+              ## location specified in aspiration_location.                 ##
             p300.dispense(dispension_vol, well)
-              ## Dispense the amount specified in dispension_vol to the         ##
-              ## location specified in well (looping through plate)             ##
+              ## Dispense the amount specified in dispension_vol to the     ##
+              ## location specified in well (looping through plate)         ##
             p300.dispense(10, aspiration_location)
-              ## Alternative for blow-out, make sure the tip doesn't fill       ##
-              ## completely when using a disposal volume by dispensing some     ##
-              ## of the volume after each pipetting step. (blow-out too many    ##
-              ## bubbles)                                                       ##
+              ## Alternative for blow-out, make sure the tip doesn't fill   ##
+              ## completely when using a disposal volume by dispensing some ##
+              ## of the volume after each pipetting step. (blow-out too many##
+              ## bubbles)                                                   ##
         p300.drop_tip()
-          ## when entire plate is full, drop tip                                ##
+          ## when entire plate is full, drop tip                            ##
 # =============================================================================
 
 
 # DILUTING AND DISTRIBUTING SAMPLES============================================
 # =============================================================================
-    for sample, dest_well, dilution_well in zip(
-            samples, 
-            sample_dest, 
-            dilution_plate
+    for sample, dilution_well, pcr_well in zip(
+            samples,
+            dilution_plate,
+            sample_dest            
             ):
-      ## Combine each sample with a dilution_well,       
+      ## Combine each sample with a dilution_well and a destination well    ##
+          p20.pick_up_tip()
+          ## p20 picks up tip from location of specified starting_tip       ##
+          ## or following                                                   ##
+          p20.aspirate(sample_vol_dil, sample)
+          ## aspirate sample_volume_dil = volume for dilution from sample   ##
+          p20.dispense(sample_vol_dil, dilution_well)
+          ## dispense sample_volume_dil = volume for dilution into dil_well ##
+          mix_vol = sample_vol_dil + 3
+          ## Set volume for mixing up and down.                             ##
+          for i in range (3):
+            p20.aspirate(mix_vol, dilution_well)
+            p20.dispense(mix_vol, dilution_well)
+              ## Mix 3 times up and down with sample volume +3.             ##
+          p20.aspirate(sample_vol_pcr, dilution_well)
+          ## aspirate sample_vol_mix = volume for in mastermix from dil_well##
+          p20.dispense(sample_vol_pcr, pcr_well)
+          ## dispense sample_vol_mix = volume for in mastermix into pcr_well##
+          mix_vol = sample_vol_pcr + 3
+          ## Set volume for mixing up and down.                             ##
+          for i in range (3):
+            p20.aspirate(mix_vol, pcr_well)
+            p20.dispense(mix_vol, pcr_well)
+              ## Mix 3 times up and down with sample volume +3.             ##
+          sample_dispense = mix_vol + 3
+          ## Set extra dispension volume after mixing to mix volume +3.     ##
+          p20.dispense(sample_dispense, pcr_well)
+          ## Dispese the mix volume + 3 in the well.                        ##
+          p20.drop_tip()
+          ## Drop tip in trashbin on 12.                                    ##
+
+# DILUTING AND DISTRIBUTING SAMPLE MIX=========================================
+# =============================================================================
+
+    #### diluting sample mix.                                               ##
+    p20.pick_up_tip()
+      ## p20 picks up tip from location of specified starting_tip or        ##
+      ## the following.                                                     ##
+    p20.aspirate(sample_vol_dil, sample_mix)
+      ## aspirate sample_volume_dil = volume for dil. from sample_mix       ##
+    p20.dispense(sample_vol_dil, plate_96_dil.wells_by_name()['A12'])
+      ## dispense sample_volume_dil = volume for dilution into dil_well     ##
+    mix_vol = sample_vol_dil + 3
+      ## Set volume for mixing up and down.                                 ##
+    for i in range (3):
+        p20.aspirate(mix_vol, plate_96_dil.wells_by_name()['A12'])
+        p20.dispense(mix_vol, plate_96_dil.wells_by_name()['A12'])
+          ## Mix 3 times up and down with sample volume +3.                 ##
+    p20.drop_tip()
+    
+    #### Distribute from dilution plate to entire column in PCR plate       ##
+    for well in sample_mix_dest:
+        p20.pick_up_tip()
+        p20.aspirate(sample_vol_pcr, plate_96_dil.wells_by_name()['A12'])
+          ## aspirate sample_vol_pcr = volume for in mastermix from dil_well##
+        p20.dispense(sample_vol_pcr, well)
+          ## dispense sample_vol_mix = volume for in mastermix into pcr_well##
+        mix_vol = sample_vol_pcr + 3
+          ## Set volume for mixing up and down.                             ##
+        for i in range (3):
+            p20.aspirate(mix_vol, well)
+            p20.dispense(mix_vol, well)
+              ## Mix 3 times up and down with sample volume +3.             ##
+        sample_dispense = mix_vol + 3
+          ## Set extra dispension volume after mixing to mix volume +3.     ##
+        p20.dispense(sample_dispense, well)
+          ## Dispese the mix volume + 3 in the well.                        ##
+        p20.drop_tip()
+          ## Drop tip in trashbin on 12.                                    ##
         
+        
+              
+               
         

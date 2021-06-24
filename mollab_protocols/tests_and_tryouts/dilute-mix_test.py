e75b9011eff399c2599d2e03f6f9cc3a984caedc
Maartje <brouwer.mm@gmail.com>
2021-06-24 16:04:52 +0200

    Update dilute-mix_test.py
    started on sample distribution

--- mollab_protocols/tests_and_tryouts/dilute-mix_test.py
+++ mollab_protocols/tests_and_tryouts/dilute-mix_test.py
@@ -246,4 +246,55 @@ def run(protocol: protocol_api.ProtocolContext):
           ## when entire plate is full, drop tip                            ##
 # =============================================================================
     
+
+# DILUTING AND DISTRIBUTING SAMPLE MIX=========================================
+# =============================================================================
+
+##### 1: Mixing 3x with sample_vol +3. Distribute in 24 wells
+
+    dilution = dilution_plate[0]
+    mix_vol = sample_vol_dil + 3
+    number_of_mixes = 3
+    destination = PCR_plate[:24]
+    
+
+    #### diluting sample                                               ##
+    p20.pick_up_tip()
+      ## p20 picks up tip from location of specified starting_tip or        ##
+      ## the following.                                                     ##
+    p20.aspirate(sample_vol_dil, sample)
+      ## aspirate sample_volume_dil = volume for dil. from sample_mix       ##
+    p20.dispense(sample_vol_dil, dilution)
+      ## dispense sample_volume_dil = volume for dilution into dil_well     ##
+    
+      ## Set volume for mixing up and down.                                 ##
+    for i in range (number_of_mixes):
+        p20.aspirate(mix_vol, dilution)
+        p20.dispense(mix_vol, dilution)
+          ## Mix 3 times up and down with sample volume +3.                 ##
+    p20.drop_tip()
+    
+    #### Distribute from dilution to PCR plate       ##
+    for well in destination:
+        p20.pick_up_tip()
+        p20.aspirate(sample_vol_pcr, dilution)
+          ## aspirate sample_vol_pcr = volume for in mastermix from dil_well##
+        p20.dispense(sample_vol_pcr, well)
+          ## dispense sample_vol_mix = volume for in mastermix into pcr_well##
+        mix_vol = sample_vol_pcr + 3
+          ## Set volume for mixing up and down.                             ##
+        for i in range (3):
+            p20.aspirate(mix_vol, well)
+            p20.dispense(mix_vol, well)
+              ## Mix 3 times up and down with sample volume +3.             ##
+        sample_dispense = mix_vol + 3
+          ## Set extra dispension volume after mixing to mix volume +3.     ##
+        p20.dispense(sample_dispense, well)
+          ## Dispese the mix volume + 3 in the well.                        ##
+        p20.drop_tip()
+          ## Drop tip in trashbin on 12.                                    ##
+        
+        
+              
+               
     

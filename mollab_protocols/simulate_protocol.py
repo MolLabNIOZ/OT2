f56ed70fedd9a370bdf433b1c2df7a394a416a05
29sv8 <73705620+29sv8@users.noreply.github.com>
2021-09-14 09:41:39 +0200

    Update simulate_protocol.py

--- mollab_protocols/simulate_protocol.py
+++ mollab_protocols/simulate_protocol.py
@@ -4,8 +4,12 @@
 from opentrons.simulate import simulate, format_runlog
 
 # Read file
+<<<<<<< Updated upstream
 protocol_file = open("OT2/mollab_protocols/PCR/qPCRre-do.py")
 
+=======
+protocol_file = open("C:/Users/svreugdenhil/Documents/GitHub/OT2/mollab_protocols/tests_and_tryouts/calibration_check.py")
+>>>>>>> Stashed changes
 
 # Simulate protocol
 runlog, _bundle = simulate(protocol_file)

4c22fb5b6afa1d4df0f873efc1281ec198d1fcb5
Maartje Brouwer <maartje.brouwer@nioz.nl>
2021-11-01 08:11:18 +0100

    Update get_uL_info.py

--- mollab_protocols/project_related/Pierre_Julia/get_uL_info.py
+++ mollab_protocols/project_related/Pierre_Julia/get_uL_info.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-volume_data = pd.read_excel("LAB_TABLE.xlsx")
+volume_data = pd.read_excel("mollab_protocols/project_related/Pierre_Julia/TEST_RUN1.xlsx")
 
 dna = volume_data['PCR1_DNA_VOL'].to_list()
 

724f8f4f4f6ab30bf7e95e8b8b707dabd082bab8
Maartje <78266071+maartje-git@users.noreply.github.com>
2021-11-24 14:53:05 +0100

    Update calibration_check_v2.py

--- mollab_protocols/tests_and_tryouts/calibration_check_v2.py
+++ mollab_protocols/tests_and_tryouts/calibration_check_v2.py
@@ -3,24 +3,32 @@
 # Description: short protocol to check if the calibration is ok.
 # =============================================================================
 
+
 # IMPORT STATEMENTS============================================================
+# =============================================================================
 from opentrons import protocol_api
   ## Import opentrons protocol API v2.                                      ##
 # =============================================================================
 
+
 # VARIABLES TO SET=============================================================
+# =============================================================================
 pipette20 = True
 p20_start_tip = 'A1'
 pipette300 = True
 p300_start_tip = 'A1'
 plate = True
-plate_on_rack = True #plate on coolrack
+plate_on_rack = True #Plate on coolrack
 strips = True
 tubes = True #Tubes 1.5mL
 tubes5mL = True
+test_wells = True #Test 3 wells spread over the labware
+test_columns = True #Test 3 columns spread over the labware
 # =============================================================================
 
+
 # RUN==========================================================================
+# =============================================================================
 ##### Metadata
 metadata = {
     'protocolName': 'Calibration check v2',
@@ -36,237 +44,121 @@ def run(protocol: protocol_api.ProtocolContext):
     """
 # =============================================================================
 
+
 # LOADING LABWARE==============================================================
+# =============================================================================
+    labwares = []
+      ## empty list to add labware to, to loop through
+    
+    ##### Loading pipettetips
     if pipette300:
         tips_200 = protocol.load_labware(
-            'opentrons_96_filtertiprack_200ul',  #labware definition
-            10,                                  #deck position
-            '200tips')                           #custom name
+            'opentrons_96_filtertiprack_200ul',  
+            10,                                  
+            '200tips')                           
     if pipette20:
         tips_20 = protocol.load_labware(
-            'opentrons_96_filtertiprack_20ul',  #labware definition
-            11,                                 #deck position
-            '20tips')                           #custom name
+            'opentrons_96_filtertiprack_20ul',  
+            11,                                 
+            '20tips')                           
+    
+    ##### Loading labware
     if plate:
         plate_96 = protocol.load_labware(
             'biorad_96_wellplate_200ul_pcr',
             1,
             '96well_plate')
+        labwares.append(plate_96)
     if plate_on_rack:
-        high_plate_96 = protocol.load_labware(
+        plate_coolrack = protocol.load_labware(
             'biorad_qpcr_plate_eppendorf_cool_rack',
             2,
-            '96well_plate_rack')
+            '96well_plate_on_rack')
+        labwares.append(plate_coolrack)
     if tubes:
         sample_tubes = protocol.load_labware(
-            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',#labware def
-            4,                                                       #deck position
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            4,                                                       
             'sample_tubes')
-    if strips:                                          #custom name
+        labwares.append(sample_tubes)
+    if strips:                                          
         pcr_strips = protocol.load_labware(
-            'pcrstrips_96_wellplate_200ul',     #labware definition
-            3,                                  #deck position
-            'pcr_strips')                       #custom name
+            'pcrstrips_96_wellplate_200ul',     
+            3,                                 
+            'pcr_strips')                       
+        labwares.append(pcr_strips)
     if tubes5mL:
         tubes_5mL = protocol.load_labware(
-            'eppendorfscrewcap_15_tuberack_5000ul', #labware definition
-            5,                                      #deck position 
-            'tubes_5mL')                            #custom name
+            'eppendorfscrewcap_15_tuberack_5000ul', 
+            5,                                      
+            'tubes_5mL')                            
+        labwares.append(tubes_5mL)
     
     ##### Loading pipettes
     if pipette300:
         p300 = protocol.load_instrument(
-            'p300_single_gen2',                  #instrument definition
-            'right',                             #mount position
-            tip_racks=[tips_200])                #assigned tiprack
+            'p300_single_gen2',                  
+            'right',                            
+            tip_racks=[tips_200])               
     if pipette20:
         p20 = protocol.load_instrument(
-            'p20_single_gen2',                  #instrument definition
-            'left',                             #mount position
-            tip_racks=[tips_20])                #assigned tiprack
+            'p20_single_gen2',                 
+            'left',                            
+            tip_racks=[tips_20])                
 # =============================================================================
 
-# PREDEFINED VARIABLES=========================================================    
+# START-UP=====================================================================    
     protocol.set_rail_lights(True)
+      ## turn on lights 
     
     if pipette300:
         p300.starting_tip = tips_200.well(p300_start_tip)
+          ## define first tip
         p300.pick_up_tip()
+          ## pick up first tip
     if pipette20:
         p20.starting_tip = tips_20.well(p20_start_tip)
+          ## define first tip
         p20.pick_up_tip()
+          ## pick up first tip
 # =============================================================================
 
 
-# test PCR plate
+# test labwares
 # =============================================================================
-    
+    for labware in labwares:
     # wells
-    aspiration_location_wells = []
-    aspiration_wells = (
-        [plate_96.wells_by_name()[well_name] for well_name in 
-          ['A1', 'A7', 'A11']])
-    for well in aspiration_wells:
-        aspiration_location_wells.append(well)
-            
-    for well in aspiration_location_wells:
-        p300.aspirate(10, well)
-        p300.dispense(10, well)
-        p300.blow_out(well)
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
+        wells = []
+        if test_wells:
+            all_wells = labware.wells()
+            total_number_of_wells = len(all_wells)
+            middle = int(total_number_of_wells / 2)
+            wells.append(all_wells[0])
+            wells.append(all_wells[middle])
+            wells.append(all_wells[-1])
+              ## pick 3 wells, nicely distributed of the labware
+        if test_columns:
+            all_columns = labware.columns()
+            total_number_of_columns = len(all_columns)
+            middle = int(total_number_of_columns / 2)
+            columns = []
+            columns.append(all_columns[0])
+            columns.append(all_columns[middle])
+            columns.append(all_columns[-1])
+              ## pick 3 columns, nicely distributed of the labware
+            for column in columns:
+                for well in column:
+                    wells.append(well)
+                    
+            for well in wells:
+                if p300:
+                    p300.move_to(well.bottom())
+                    protocol.delay(seconds=2)
+            for well in wells:
+                if p20:
+                    p20.move_to(well.bottom()) 
+                    protocol.delay(seconds=2)
 
-    protocol.pause('was this ok?')  
-    
-    # columns
-    aspiration_location_columns = []
-    aspiration_columns = (
-        [plate_96.columns_by_name()[column_name] for column_name in
-          ['2', '6', '12']]
-        )
-    for column in aspiration_columns:
-        for well in column:
-            aspiration_location_columns.append(well)
-    
-    for well in aspiration_location_columns:
-        p300.aspirate(10, well)
-        p300.dispense(10, well)
-        p300.blow_out(well)
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
-
-    protocol.pause('was this ok?')    
-
-# =============================================================================
-# test PCR plate + rack
-# =============================================================================
-    
-    # wells
-    aspiration_location_wells = []
-    aspiration_wells = (
-        [high_plate_96.wells_by_name()[well_name] for well_name in 
-          ['A1', 'A7', 'A11']])
-    for well in aspiration_wells:
-        aspiration_location_wells.append(well)
-            
-    for well in aspiration_location_wells:
-        p300.aspirate(10, well)
-        p300.dispense(10, well)
-        p300.blow_out(well)
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
-    
-    protocol.pause('was this ok?')    
-    
-    # columns
-    aspiration_location_columns = []
-    aspiration_columns = (
-        [high_plate_96.columns_by_name()[column_name] for column_name in
-          ['2', '6', '12']]
-        )
-    for column in aspiration_columns:
-        for well in column:
-            aspiration_location_columns.append(well)
-    
-    for well in aspiration_location_columns:
-        p300.aspirate(10, well)
-        p300.dispense(10, well)
-        p300.blow_out(well)
-    for well in aspiration_location_columns:
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
-
-    protocol.pause('was this ok?')    
-
-
-# =============================================================================
-# test 1.5mL tubes
-# =============================================================================
-    aspiration_location = []
-    aspiration_columns = (
-        [sample_tubes.columns_by_name()[column_name] for column_name in 
-          ['1', '4', '6']]
-        )
-    for column in aspiration_columns:
-        for well in column:
-            aspiration_location.append(well)
-    
-    for well in aspiration_location:
-        # p300.aspirate(10, well)
-        # p300.dispense(10, well)
-        # p300.blow_out(well)
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
-
-    protocol.pause('was this ok?')    
-    
-# =============================================================================
-# test PCR tube strips
-# =============================================================================
-    
-    # wells
-    aspiration_location_wells = []
-    aspiration_wells = (
-        [pcr_strips.wells_by_name()[well_name] for well_name in 
-          ['A1', 'A7', 'A11']])
-    for well in aspiration_wells:
-        aspiration_location_wells.append(well)
-            
-    for well in aspiration_location_wells:
-        p300.aspirate(10, well)
-        p300.dispense(10, well)
-        p300.blow_out(well)
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
-
-    # protocol.pause('was this ok?')  
-    
-    columns
-    aspiration_location_columns = []
-    aspiration_columns = (
-        [pcr_strips.columns_by_name()[column_name] for column_name in
-          ['1', '7', '11']]
-        )
-    for column in aspiration_columns:
-        for well in column:
-            aspiration_location_columns.append(well)
-    
-    for well in aspiration_location_columns:
-        p300.aspirate(10, well)
-        p300.dispense(10, well)
-        p300.blow_out(well)
-        p20.aspirate(10, well)
-        p20.dispense(10, well)
-        p20.blow_out(well)
-
-    protocol.pause('was this ok?')       
-    
-# =============================================================================
-# test 5mL (screw cap) tube
-# =============================================================================
-    p300.aspirate(10, tubes_5mL['A1'])
-    p300.dispense(10, tubes_5mL['A1'])
-    p300.blow_out(tubes_5mL['A1'])
-    p20.aspirate(10, tubes_5mL['A1'])
-    p20.dispense(10, tubes_5mL['A1'])
-    p20.blow_out(tubes_5mL['A1'])
-    
-    p300.aspirate(10, tubes_5mL['C5'])
-    p300.dispense(10, tubes_5mL['C5'])
-    p300.blow_out(tubes_5mL['C5'])
-    p20.aspirate(10, tubes_5mL['C5'])
-    p20.dispense(10, tubes_5mL['C5'])
-    p20.blow_out(tubes_5mL['C5'])
-    
-
-    protocol.pause('was this ok?')    
- 
     
 # =============================================================================
 # =============================================================================

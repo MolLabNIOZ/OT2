9db4454726717335d9d83ad437d7264a4c2e1141
Sanne Vreugdenhil <sanne.vreugdenhil@nioz.nl>
2021-12-15 09:06:13 +0100

    Update general_illumina_PCR_WALL-E.py
    clean up

--- mollab_protocols/PCR/general_illumina_PCR_WALL-E.py
+++ mollab_protocols/PCR/general_illumina_PCR_WALL-E.py
@@ -5,6 +5,8 @@ of mastermix and barcoded primers to a 96-wells plate
 You have to provide:
     Number of samples (excl. NTC and mock)
     Number of NTCs 
+        NOTE: The NTC should ALWAYS be at the end of the plate, this is
+        for the protocol for EVE (who skips the last well)
     Do you want the robot to add a mock sample?:
         if True -- robot adds an extra well + primer combination
     Whether you are doing a qPCR or not 
@@ -27,7 +29,6 @@ When not doing a qPCR:
     Lights are turned on and off
     Mastermix is aliquoted from the mastermix tube into the plate
     Primers are added from the primer source to the plate 
-        NOTE: 1 strip per column in the plate!!
 
 In addition for the qPCR:
     The lights are not turned on and off
@@ -51,10 +52,6 @@ Updates:
 """
 # VARIABLES TO SET#!!!=========================================================
 # =============================================================================
-# Do you want to simulate the protocol?
-simulate = False
-  ## True for simulating protocol, False for robot protocol                 ##
-
 # What is the starting position of the 20µL tips?
 starting_tip_p20 = 'A1'
 # If mastermix dispense > 19: What is the starting position of the 200µL tips?
@@ -63,94 +60,100 @@ starting_tip_p200 = 'A1'
   
 # How many samples do you want to include?
 number_of_samples = 6   
-  ## If NOT qPCR and NOT mock                                MAX == 95      ##
-  ## If NOT qPCR but incl. mock                              MAX == 94      ##
-  ## If qPCR    MAX ==  number of samples -                                 ##
-  ##                    (number of std series * length of std series) -     ##
-  ##                    number of standard sample replicates                ##
+  ## If NOT qPCR and NOT mock                                MAX == 95      
+  ## If NOT qPCR but incl. mock                              MAX == 94      
+  ## If qPCR    MAX ==  number of samples -                                 
+  ##                    (number of std series * length of std series) -     
+  ##                    number of standard sample replicates                
 
 # How many NTCs to include 
 number_of_NTCs = 1 
+  ## NOTE: The NTC should ALWAYS be at the end of your plate!!              
 
 # Will a mock sample be included?
 mock = False
-  ## True if mock has to be added by the robot.                             ##
-  ## False if mock is not added by the robot.                               ##
+  ## True if mock has to be added by the robot.                             
+  ## False if mock is not added by the robot.                               
 
 # What is the total volume (µL) of your mix?
 start_vol = 1108.8
-  ## The start_vol_m is the volume (µL) of mix that is in the source        ##
-  ## labware at the start of the protocol.                                  ##
+  ## The start_vol_m is the volume (µL) of mix that is in the source        
+  ## labware at the start of the protocol.                                  
   
 # Are you doing a qPCR or a regular PCR?
 qPCR = False
-  ## True or False                                                          ##
-  ## Lights off if qPCR, standard sample and/or standard dilution series    ##                                    ##
+  ## True or False                                                          
+  ## Lights off if qPCR, standard sample and/or standard dilution series                                        ##
 if qPCR:
     # How many dilution serie replicates do you want to include?
     number_of_std_series = 1  
     # How many dilutions are in the standard dilution series?
     length_std_series = 8  
-      ## length_of_std_series  MAX == 8                                     ##
-    # How many replicates of the stax 8ndard sample are you taking?
+      ## length_of_std_series  MAX == 8                                     
+    # How many replicates of the standard sample are you taking?
     number_of_std_samples = 6
     # In what well is the primer for the standards (sample and series) located?
     std_primer_loc = 'D1'
                    
 else:
-    ## If we are not doing a qPCR - protocol uses these values.             ##
+    ## If we are not doing a qPCR - protocol uses these values.             
     number_of_std_series = 0  
     length_std_series = 0
     number_of_std_samples = 0
 
 # Which tube are you using for your mastermix? (options 1.5mL or 5mL)
 mastermix_tube_type = '1.5mL_tube'
-  ## For volume < 1300: '1.5mL_tube'                                        ##
-  ## For volume > 1300: '5mL_tube'                                          ##
+  ## For volume < 1300: '1.5mL_tube'                                        
+  ## For volume > 1300: '5mL_tube'                                          
 
-# What volume (µL) of mastermix that needs to be dispensed?
+# What is the volume (µL) of mastermix that needs to be dispensed?
 dispension_vol = 19   
-  
-if mastermix_tube_type == '1.5mL_tube':
-    mastermix_source = 'D1'
-if mastermix_tube_type == '5mL_tube':
-    mastermix_source = 'C1'
-  ## Where is the mastermix tube located in the rack                        ##
-  
+
+# Where is the mastermix tube located in the rack? 
+mastermix_source = 'C1'
+  ## convenient places:
+  ## if mastermix_tube_type ==   '1.5mL_tube'  -->  D1 
+  ## if mastermix_tube_type ==   '5mL_tube'    -->  C1
+    
+# What labware are your primers in?
 primer_tube_type = 'PCR_strips'
-  ## Where are your primers in?                                             ##
-  ## Primers in strips = 'PCR_strips'                                       ##
-  ## Primers in plate = 'plate_96'                                          ##
+  ## Primers in strips = 'PCR_strips'                                       
+  ## Primers in plate = 'plate_96'                                          
+# What is the volume (µL) of primer that needs to be added to the mix?
 primer_vol = 1
-  ## Volume of the primer (F+R mix) to be used                              ##
+# In which columns are the strips in the plate (ignore if not using strips)?
 primer_loc = ['2', '5', '8','11']
-  ## Location of the primer strips in the plate                             ##
+  ## optional: ['2', '7', '11'] or ['2', '5', '8','11']                     
+  ## max 4 racks with strips!                                               
+  
+# Do you want to simulate the protocol?
+simulate = False
+  ## True for simulating protocol, False for robot protocol                 
 # =============================================================================
 
 # IMPORT STATEMENTS============================================================
 # =============================================================================
 from opentrons import protocol_api
-  ## Import opentrons protocol API v2.                                      ##
+  ## Import opentrons protocol API v2.                                      
 import math
   ## To do some calculations (rounding up) 
-import json 
-  ## Import json to import custom labware with labware_from_definition,     ##
-  ## so that we can use the simulate_protocol with custom labware.          ##
-
+  
 if simulate: #Simulator
     from mollab_modules import volume_tracking_v1 as vt
+    import json 
+      ## Import json to import custom labware with labware_from_definition,
+      ## so that we can use the simulate_protocol with custom labware.     
 else: #Robot
     from data.user_storage.mollab_modules import volume_tracking_v1 as vt
-
 # =============================================================================
 
 # CALCULATED VARIABLES=========================================================
 # =============================================================================
 number_of_primers = number_of_samples + number_of_NTCs
-  ## Calculate how many unique primer pairs are needed.                     ##
+  ## Calculate how many unique primer pairs are needed.                    
 if mock:
     number_of_primers = number_of_primers + 1    
-      ## If a mock is used, there should be 1 more unique primer pair.      ##
+      ## If a mock is used, there should be 1 more unique primer pair.     
       
 if primer_tube_type == 'PCR_strips':
     primer_racks = math.ceil(number_of_primers / 32)  
@@ -184,8 +187,8 @@ def run(protocol: protocol_api.ProtocolContext):
 # =============================================================================
     # Pipette tips
     if dispension_vol >= 19:
-      ## When the mm volume to be dispensed >= 19, 200µL tips are           ##
-      ## needed in addition to the 20µL tips.                               ##
+      ## When the mm volume to be dispensed >= 19, 200µL tips are          
+      ## needed in addition to the 20µL tips.                              
         tips_200 = protocol.load_labware(
             'opentrons_96_filtertiprack_200ul', 
             2,                                  
@@ -200,7 +203,7 @@ def run(protocol: protocol_api.ProtocolContext):
             '20tips_2')                         
         tips_20 = [tips_20_1, tips_20_2]
     else:
-      ## When the mm volume to be dispensed <=19, only 20µL are needed      ##
+      ## When the mm volume to be dispensed <=19, only 20µL are needed      
         tips_20_1 = protocol.load_labware(
             'opentrons_96_filtertiprack_20ul',  
             2,                                  
@@ -235,8 +238,7 @@ def run(protocol: protocol_api.ProtocolContext):
         primer_source_1 = protocol.load_labware(
             'biorad_96_wellplate_200ul_pcr',    
             1,                                  
-            'primer_source_1')                     
-        
+            'primer_source_1')                         
                         
     if simulate: #Simulator
         if mastermix_tube_type == '5mL_tube': 
@@ -302,23 +304,23 @@ def run(protocol: protocol_api.ProtocolContext):
 # PREDIFINED VARIABLES=========================================================
 # =============================================================================
     aspiration_vol = dispension_vol + (dispension_vol/100*2)
-      ## The aspiration_vol is the volume (µL) that is aspirated from the   ##
-      ## container.                                                         ##
+      ## The aspiration_vol is the volume (µL) that is aspirated from the   
+      ## container.                                                         
     ##### Variables for volume tracking
     start_height = vt.cal_start_height('tube_5mL', start_vol)
-      ## Call start height calculation function from volume tracking module.##
+      ## Call start height calculation function from volume tracking module.
     current_height = start_height
-      ## Set the current height to start height at the beginning of the     ##
-      ## protocol.                                                          ##
+      ## Set the current height to start height at the beginning of the     
+      ## protocol.                                                          
     primer_mix_vol = primer_vol + 3
-      ## primer_mix_vol = volume for pipetting up and down                  ##
+      ## primer_mix_vol = volume for pipetting up and down                  
 # =============================================================================
 
 # SETTING LOCATIONS============================================================
 # =============================================================================
     # Setting starting tip                                           
     if dispension_vol >= 19:
-        ## If the mm volume to be dispendsed >= 19, assign p300 starting tip##
+        ## If the mm volume to be dispendsed >= 19, assign p300 starting tip
         p300.starting_tip = tips_200.well(starting_tip_p200)
     p20.starting_tip = tips_20_1.well(starting_tip_p20)
     
@@ -332,16 +334,16 @@ def run(protocol: protocol_api.ProtocolContext):
     
     # Create the list of wells where samples should go
     sample_wells = wells[:number_of_primers]
-      ## cuts off the list after a the number_of_samples number of wells    ##
+      ## cuts off the list after a the number_of_samples number of wells    
     
     # Create the list of wells where standard sample replicates should go
     slice_std_wells = slice(
         number_of_primers, number_of_primers + number_of_std_samples)
-      ## Slice the list - I need the number_of_std_samples of wells after   ##
-      ## the number_of_samples wells. So I need a certain amount of wells   ##
-      ## after the sample wells, but not all of the wells after that.       ##
-      ## This slices the list after the samples and after the std sample    ##
-      ## so that we only take the wells in between.                         ##
+      ## Slice the list - I need the number_of_std_samples of wells after   
+      ## the number_of_samples wells. So I need a certain amount of wells   
+      ## after the sample wells, but not all of the wells after that.       
+      ## This slices the list after the samples and after the std sample    
+      ## so that we only take the wells in between.                         
     std_sample_wells = wells[slice_std_wells]
 
     # Create the lsit of wells where standerd series should go
@@ -350,17 +352,17 @@ def run(protocol: protocol_api.ProtocolContext):
     [plate_96.columns_by_name()[column_name] for column_name in
      ['12', '11', '10']])
     std_series_columns = std_series_columns[:number_of_std_series]
-    ## Reserve a column at the end of the plate for every std_series        ##
-    ## Separate the columns into wells and append them to list              ##
+    ## Reserve a column at the end of the plate for every std_series        
+    ## Separate the columns into wells and append them to list              
     for column in std_series_columns:
         column = column[:length_std_series]
-        ## cut off the columns after a certain std_series length            ##
+        ## cut off the columns after a certain std_series length            
         for well in column:
             std_series_wells.append(well)
 
     # Add the wells of the standards into 1 list 
-      ## This needs to be a list separate from the rest of the wells because##
-      ## they need the same primer from a separate tube.                    ##
+      ## This needs to be a list separate from the rest of the wells because
+      ## they need the same primer from a separate tube.                    
     std_wells = std_sample_wells + std_series_wells 
     # Add all the wells that need mastermix into 1 list
     MasterMixAliquots = sample_wells + std_wells
@@ -387,7 +389,7 @@ def run(protocol: protocol_api.ProtocolContext):
                   for column_name in primer_loc]))
             for column in primer_columns_3:
                 primer_columns.append(column)
-            ## Make a list of columns, this is a list of lists!                 ##
+            ## Make a list of columns, this is a list of lists!             
         for column in primer_columns:
             for well in column:
                 primer_wells.append(well)
@@ -420,46 +422,46 @@ def run(protocol: protocol_api.ProtocolContext):
     else:
         pipette = p20
     for i, well in enumerate(MasterMixAliquots):
-      ## aliquot mix, for each well do the following:                       ##
+      ## aliquot mix, for each well do the following:                       
         if i == 0: 
             pipette.pick_up_tip()
-              ## If we are at the first well, start by picking up a tip.    ##
+              ## If we are at the first well, start by picking up a tip.    
         elif i % 8 == 0:
             pipette.drop_tip()
             pipette.pick_up_tip()
-              ## Then, after every 8th well, drop tip and pick up new       ##
+              ## Then, after every 8th well, drop tip and pick up new       
     
         current_height, pip_height, bottom_reached = vt.volume_tracking(
                 'tube_5mL', dispension_vol, current_height)
-                  ## call volume_tracking function, obtain current_height,  ##
-                  ## pip_height and whether bottom_reached.                 ##
+                  ## call volume_tracking function, obtain current_height,  
+                  ## pip_height and whether bottom_reached.                 
         
         if bottom_reached:
             aspiration_location = MasterMix.bottom(z=1)
             protocol.comment("You've reached the bottom of the tube!")
-              ## If bottom is reached keep pipetting from bottom + 1        ##
+              ## If bottom is reached keep pipetting from bottom + 1        
         else:
             aspiration_location = MasterMix.bottom(pip_height)
-              ## Set the location of where to aspirate from.                ##
+              ## Set the location of where to aspirate from.                
 
-        #### The actual aliquoting of mastermix                             ##
+        #### The actual aliquoting of mastermix                             
         pipette.aspirate(aspiration_vol, aspiration_location)
-          ## Aspirate the amount specified in aspiration_vol from the       ##
-          ## location specified in aspiration_location.                     ##
+          ## Aspirate the amount specified in aspiration_vol from the       
+          ## location specified in aspiration_location.                     
         pipette.dispense(dispension_vol, well)
-          ## Dispense the amount specified in dispension_vol to the         ##
-          ## location specified in well (so a new well every time the       ##
-          ## loop restarts)                                                 ##
+          ## Dispense the amount specified in dispension_vol to the         
+          ## location specified in well (so a new well every time the       
+          ## loop restarts)                                                 
         pipette.dispense(10, aspiration_location)
-          ## Alternative for blow-out, make sure the tip doesn't fill       ##
-          ## completely when using a disposal volume by dispensing some     ##
-          ## of the volume after each pipetting step. (blow-out to many     ##
-          ## bubbles)                                                       ##
+          ## Alternative for blow-out, make sure the tip doesn't fill      
+          ## completely when using a disposal volume by dispensing some     
+          ## of the volume after each pipetting step. (blow-out to many     
+          ## bubbles)                                                       
     pipette.drop_tip()   
 ## ----------------------------------------------------------------------------
 ## ADDING PRIMERS FOR SAMPLES TO THE MIX---------------------------------------
     for primer_well, sample_well in zip(primer_wells, sample_wells):
-      ## Loop trough primer_wells and sample_wells                          ##
+      ## Loop trough primer_wells and sample_wells                          
         p20.pick_up_tip()
         p20.aspirate(primer_vol, primer_well)
         p20.dispense(primer_vol, sample_well)

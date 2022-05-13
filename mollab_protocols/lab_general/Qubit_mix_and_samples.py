9a3b3009a98d034d2fb3fdb453d9fe6f101738a6
Sanne Vreugdenhil <sanne.vreugdenhil@nioz.nl>
2022-05-13 16:33:36 +0200

    Update Qubit_mix_and_samples.py
    labware is toegevoegd
    het onderscheid tussen 1 of 2 mixen is verder uitgebouwd
    De predifined variables zijn toegevoegd, ook de gene die nodig zijn voor de volume tracking
    Er is een begin gemaakt met het setten van locaties, hier moet op worden voortgeborduurd (zie docstring begin protocol)

--- mollab_protocols/lab_general/Qubit_mix_and_samples.py
+++ mollab_protocols/lab_general/Qubit_mix_and_samples.py
@@ -14,14 +14,25 @@ een buis met zoveel startvolume moeten neerzetten (+ 10% extra)
 licht moet UIT voor het protocol begint
 
 1.5 mL tubes met standaard (50uL aliquots maken, dan kan het zonder vol track)
+
+Probleem: hoe ga ik om met 2 tubes met een verschillend start volume?
+Dat is op zich gefixt denk ik door if mix = 1 dan is start height
+en if mix =2 dan startheight...
+
+Volgende probleem - hoe om te gaan met if 1 assay: hele plaat moet vol vanuit
+4 rekjes, if 2 assays plaat moet vol vanuit meerdere rekjes maar wel startend
+vanaf de een ander rekje -- oplossen in de locaties denk ik? start sample
+reeks a start sample reeks b, sample source moet wel hetzelfde zijn voor
+beide labwares, je moet ook kunnen kiezen dat je dezelfde samples
+met 2 verschillende assays runt!
 """
 
-# VARIABLES TO SET#!!!=========================================================
+#%% VARIABLES TO SET#!!!=========================================================
 # =============================================================================
 # What is the starting position of the 20µL tips?
 starting_tip_p20 = 'A1'
 # What is the starting position of the 200µL tips?
-starting_tip_p200 = 'A1'
+starting_tip_p300 = 'A1'
     ## If not applicable, you do not have to change anything
     
 # Are you running 1 or 2 assays at the same time?
@@ -31,24 +42,20 @@ starting_tip_p200 = 'A1'
 number_of_assays = 1
 
 # How many samples do you want to include?
-number_of_samples = 10
-# What is the starting volume 
+number_of_samples_Qmix_1 = 10
+number_of_samples_Qmix_2 = 5
 
 # Which tube are you using for your Qubit mix? (options 1.5mL or 5mL)
   ## For volume < 1300: 'tube_1.5mL'                                        
   ## For volume > 1300: 'tube_5mL'  
   ## If you are doing 2 assays in one plate, it might be that they are not
   ## in the same type of tube
+  ## The tube should also be in well A1 of the labware!!!
 if number_of_assays == 1:
-    Qmix_tube_type_left = 'tube_5mL'
+    Qmix_tube_type_1 = 'tube_5mL'
 if number_of_assays == 2: 
-    Qmix_tube_type_1_left = 'tube_5mL'
-    Qmix_tube_tupe_2_right = 'tube_1.5mL'
-
-mastermix_source = 'C1'
-  ## convenient places:
-  ## if mastermix_tube_type ==   'tube_1.5mL'  -->  D1 
-  ## if mastermix_tube_type ==   'tube_5mL'    -->  C1
+    Qmix_tube_type_1 = 'tube_5mL'
+    Qmix_tube_type_2 = 'tube_1.5mL'
 
 # What labware are your samples in?
 sample_tube_type = 'PCR_strip'  
@@ -73,7 +80,7 @@ simulate = True
   ## True for simulating protocol, False for robot protocol
 # =============================================================================
 
-# IMPORT STATEMENTS============================================================
+#%% IMPORT STATEMENTS============================================================
 # =============================================================================
 from opentrons import protocol_api
   ## Import opentrons protocol API v2.
@@ -92,7 +99,7 @@ if simulate: #Simulator
 else: #Robot
     from data.user_storage.mollab_modules import volume_tracking_v1 as vt
   
-# CALCULATED AND SET VARIABLES=================================================
+#%% CALCULATED AND SET VARIABLES=================================================
 # =============================================================================
 # Setting callibration data per labware for both robot and simulator
 if simulate: #Simulator
@@ -103,7 +110,10 @@ else: #Robot
     offsets = pd.read_csv(
         "data/user_storage/mollab_modules/labware_offset.csv", sep=';')
     offsets = offsets.set_index('labware')
-    
+
+# In which well is your mix tube? 
+Qmix_source = 'A1'
+
 # Dispension volume of the mix for the standards
 dispension_vol_std = 48
 
@@ -116,6 +126,15 @@ std_vol = 2
 # Volume of the sample to add
 sample_vol = 1
 
+start_vol_Qmix_1 = (
+    (number_of_samples_Qmix_1*dispension_vol_sample) + 
+    (8*dispension_vol_std)
+    )
+start_vol_Qmix_2 = (
+    (number_of_samples_Qmix_2*dispension_vol_sample) +
+    (8*dispension_vol_std)
+    )
+
 if sample_tube_type == 'tube_1.5mL':
     samples_per_rack = 24
 if sample_tube_type == 'plate_96':
@@ -126,7 +145,7 @@ sample_racks = math.ceil(number_of_samples / samples_per_rack)
   ## How many tube_strip_racks are needed (1,2 or 3)
 # =============================================================================
 
-# METADATA=====================================================================
+#%% METADATA=====================================================================
 # =============================================================================
 metadata = {
     'protocolName': 'Qubit - mix and samples',
@@ -148,24 +167,199 @@ def run(protocol: protocol_api.ProtocolContext):
     # Pipette tips
     tips_200 = protocol.load_labware(
         'opentrons_96_filtertiprack_200ul',
-        1,
+        10,
         '200tips')
     tips_20_1 = protocol.load_labware(
         'opentrons_96_filtertiprack_20ul',
-        4,
+        8,
         '20tips_1')
     tips_20_2 = protocol.load_labware(
         'opentrons_96_filtertiprack_20ul',
-        7,
+        11,
         '20tips_2')
+    tips_20 = [tips_20_1, tips_20_2]
     
     # Tube racks & plates
     destination_plate = protocol.load_labware(
         'biorad_qpcr_plate_nioz_plateholder',
-        3,
+        7,
         '96well_plate')
     
-    if mastermix_tube_type == 'tube_1.5mL':
-        mastermix_tube = 
+    if number_of_assays == 1:
+        if Qmix_tube_type_1 == 'tube_1.5mL':
+            Qmix_tube_1 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            1,
+            'Qubit_mix_tube_1')
+        if Qmix_tube_type_1 == 'tube_5mL':
+            if simulate:
+                with open("labware/eppendorfscrewcap_15_tuberack_5000ul/"
+                     "eppendorfscrewcap_15_tuberack_5000ul.json") as labware_file:
+                          labware_def_5mL = json.load(labware_file)
+                Qmix_tube_1 = protocol.load_labware_from_definition( 
+                    labware_def_5mL,           
+                    1,                         
+                    'Qubit_mix_tube_1')
+            else:
+                Qmix_tube_1= protocol.load_labware(
+                    'eppendorfscrewcap_15_tuberack_5000ul',
+                    1,                                     
+                    'Qubit_mix_tube_1') 
+    if number_of_assays == 2:
+        if Qmix_tube_type_1 == 'tube_1.5mL':
+            Qmix_tube_1 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            4,
+            'Qubit_mix_tube_1')
+        if Qmix_tube_type_2 == 'tube_1.5mL':
+            Qmix_tube_2 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            4,
+            'Qubit_mix_tube_2')
+        if Qmix_tube_type_2 == 'tube_5mL':
+            if simulate:
+                with open("labware/eppendorfscrewcap_15_tuberack_5000ul/"
+                     "eppendorfscrewcap_15_tuberack_5000ul.json") as labware_file:
+                          labware_def_5mL = json.load(labware_file)
+                Qmix_tube_2 = protocol.load_labware_from_definition( 
+                    labware_def_5mL,           
+                    4,                         
+                    'Qubit_mix_tube_2')
+            else:
+                Qmix_tube_2 = protocol.load_labware(
+                    'eppendorfscrewcap_15_tuberack_5000ul',
+                    4,                                     
+                    'Qubit_mix_tube_2') 
+        if Qmix_tube_type_1 == 'tube_5mL':
+            if simulate:
+                with open("labware/eppendorfscrewcap_15_tuberack_5000ul/"
+                     "eppendorfscrewcap_15_tuberack_5000ul.json") as labware_file:
+                          labware_def_5mL = json.load(labware_file)
+                Qmix_tube_1 = protocol.load_labware_from_definition( 
+                    labware_def_5mL,           
+                    1,                         
+                    'Qubit_mix_tube_1')
+            else:
+                Qmix_tube_1 = protocol.load_labware(
+                    'eppendorfscrewcap_15_tuberack_5000ul',
+                    1,                                     
+                    'Qubit_mix_tube_1') 
+    
+    if sample_tube_type == 'tube_1.5mL':
+        sample_source_1 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            2,
+            'sample_source_1')
+        sample_source_2 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            3,
+            'sample_source_2')
+        sample_source_3 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            5,
+            'sample_source_3')
+        sample_source_4 = protocol.load_labware(
+            'opentrons_24_tuberack_eppendorf_1.5ml_safelock_snapcap',
+            6,
+            'sample_source_4')
+    if sample_tube_type == 'plate_96':
+        sample_source_1 = protocol.load_labware(
+            'biorad_96_wellplate_200ul_pcr',    
+            2,                                  
+            'sample_source_1')
+        sample_source_2 = protocol.load_labware(
+            'biorad_96_wellplate_200ul_pcr',    
+            3,                                  
+            'sample_source_2')
+    
+    if simulate: #Simulator
+        if sample_tube_type == 'PCR_strip':
+            with open("labware/pcrstrips_96_wellplate_200ul/"
+                      "pcrstrips_96_wellplate_200ul.json") as labware_file:
+                    labware_def_pcrstrips = json.load(labware_file)
+            sample_source_1 = protocol.load_labware_from_definition( 
+                labware_def_pcrstrips,     
+                2,                         
+                'sample_source_1')         
+            sample_source_2 = protocol.load_labware_from_definition( 
+                labware_def_pcrstrips, 
+                3,                     
+                'sample_source_2')    
+            sample_source_3 = protocol.load_labware_from_definition( 
+                labware_def_pcrstrips,
+                5,                   
+                'sample_source_3')    
+            sample_source_4 = protocol.load_labware_from_definition( 
+                labware_def_pcrstrips,
+                6,                   
+                'sample_source_4')     
+    else: 
+        if sample_tube_type == 'PCR_strips':
+            sample_source_1 = protocol.load_labware( 
+                'pcrstrips_96_wellplate_200ul',        
+                2,                                     
+                'sample_source_1')                      
+            sample_source_2 = protocol.load_labware_( 
+                'pcrstrips_96_wellplate_200ul',    
+                3,                                 
+                'sample_source_2')                 
+            sample_source_3 = protocol.load_labware( 
+                'pcrstrips_96_wellplate_200ul',    
+                5,                                
+                'sample_source_3') 
+            sample_source_4 = protocol.load_labware( 
+                'pcrstrips_96_wellplate_200ul',    
+                6,                                
+                'sample_source_4') 
+   
+    # Pipettes
+    p300 = protocol.load_instrument(
+        'p300_single_gen2',             
+        'right',                        
+        tip_racks=[tips_200])           
+    p20 = protocol.load_instrument(
+        'p20_single_gen2',                  
+        'left',                             
+        tip_racks=tips_20)
+# =============================================================================
 
+# PREDIFINED VARIABLES=========================================================
+# =============================================================================
+    aspiration_vol_std = (dispension_vol_std + (dispension_vol_std/100*2))
+    aspiraton_vol_sample = (
+        dispension_vol_sample + (dispension_vol_sample/100*2))
+      ## The aspiration_vol is the volume (µL) that is aspirated from the   
+      ## container.                                                         
+    
+    ##### Variables for volume tracking
+    if Qmix_tube_type_1:
+        start_height = vt.cal_start_height(Qmix_tube_type_1, start_vol_Qmix_1)
+          ## Call start height calculation function from volume tracking module.
+        current_height = start_height
+          ## Set the current height to start height at the beginning of the     
+          ## protocol.      
+    if Qmix_tube_type_2:
+        start_height = vt.cal_start_height(Qmix_tube_type_2, start_vol_Qmix_2)
+          ## Call start height calculation function from volume tracking module.
+        current_height = start_height
+          ## Set the current height to start height at the beginning of the     
+          ## protocol.   
+    std_mix_vol = std_vol + 3
+    sample_mix_vol = sample_vol + 3
+      ## mix_vol = volume for pipetting up and down                  
+# =============================================================================                   
 
+# SETTING LOCATIONS============================================================
+# =============================================================================
+    # Setting starting tip
+    p300.starting_tip = tips_200.well(starting_tip_p300)
+    p20.starting_tip = tips_20_1.well(starting_tip_p20)
+    
+    # Qubit mix location
+    if number_of_assays == 1:
+        QubitMix1 = Qmix_tube_1[Qmix_source]
+    if number_of_assays == 2:
+        QubitMix1 = Qmix_tube_1[Qmix_source]
+        QubitMix2 = Qmix_tube_2[Qmix_source]
+    
+    

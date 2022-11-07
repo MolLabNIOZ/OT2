3cf8da48ab91c934833ae42f02ac0f579a8bf2c4
Sanne Vreugdenhil <sanne.vreugdenhil@nioz.nl>
2022-11-07 14:23:34 +0100

    Update simulate_protocol.py

--- mollab_protocols/simulate_protocol.py
+++ mollab_protocols/simulate_protocol.py
@@ -4,7 +4,7 @@
 from opentrons.simulate import simulate, format_runlog
 
 # Read file
-protocol_file = open("C:/Users/svreugdenhil/Documents/GitHub/OT2/mollab_protocols/primer_aliquots/illu_primer.py")
+protocol_file = open("//zeus.nioz.nl/mmb/molecular_ecology/data_from_lab_instruments/Opentrons_robots/EVE/EVE_protocols_ran_for_labusers/2022_EVE/Eva/3different(q)PCR_EVE_no_mm.py")
 
 
 # Simulate protocol

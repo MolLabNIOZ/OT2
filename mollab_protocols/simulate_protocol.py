46e813c268b1646b0d4f625dc71611687ceb835a
Maartje <brouwer.mm@gmail.com>
2024-01-15 14:34:02 +0100

    Update simulate_protocol.py

--- mollab_protocols/simulate_protocol.py
+++ mollab_protocols/simulate_protocol.py
@@ -4,11 +4,8 @@
 from opentrons.simulate import simulate, format_runlog
 
 # Read file
-<<<<<<< HEAD
 protocol_file = open("molecular_tools/transfering of volume/from epje to PCR-plate.py")
-=======
-protocol_file = open("C:/Users/mbrouwer/OneDrive - NIOZ/Documenten/GitHub/OT2/mollab_protocols/lab_general/row_fill_plate.py")
->>>>>>> d534a77c9a327838e41bd4addd877047fef5421a
+
 
 
 # Simulate protocol

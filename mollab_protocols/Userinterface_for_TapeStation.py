be4ba2b074ca3f854dfa3e8ed97d8bb4a2182bd1
rdebeer21 <rob.de.beer@nioz.nl>
2024-02-06 16:05:38 +0100

    Create Userinterface_for_TapeStation.py

---
+++ mollab_protocols/Userinterface_for_TapeStation.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 14:41:22 2024
+@author: rdebeer
+"""
+# Import the modules you need
+from data.user_storage.mollab_modules import MO_protocols as MO
+from opentrons import protocol_api
+
+# How many samples do you want to check on the TapeStation
+number_of_reactions = 96
+
+# Wich kit are you going to use?
+tapestation_kit = 'D1000'
+    # Options are:
+    # 'D1000', 'D5000', 'gDNA', 'RNA'
+
+# What is the starting tip of the p20?    
+starting_tip = 'A1'
+
+# What are the sample indexen you want to skip?
+skipped_wells = []
+
+# Do you simulate the protocol on the PC?
+simulate = True
+
+metadata = {
+    'protocolName': 'TapeStation_protocol',
+    'author': 'MB <maartje.brouwer@nioz.nl>, RDB <rob.de.beer@nioz.nl>',
+    'description': ('Adding the reagent buffer and sample to a TapeStation-plate'),
+    'apiLevel': '2.13'}
+
+def run(protocol: protocol_api.ProtocolContext):
+    """
+    Aliquoting mastermix;
+    Adding primers from different labware.
+    """
+    
+    MO.tapestation(tapestation_kit,
+                starting_tip,
+                number_of_reactions,
+                skipped_wells,
+                simulate,
+                protocol)

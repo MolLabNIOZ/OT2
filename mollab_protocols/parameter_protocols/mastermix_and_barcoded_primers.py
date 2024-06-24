1cf2dee0e79c6ca1e2d30a780b46f2e62e191e51
Maartje <brouwer.mm@gmail.com>
2024-06-24 12:01:05 +0200

    Update mastermix_and_barcoded_primers.py

--- mollab_protocols/parameter_protocols/mastermix_and_barcoded_primers.py
+++ mollab_protocols/parameter_protocols/mastermix_and_barcoded_primers.py
@@ -4,12 +4,14 @@
 simulate = True
 #### Import opentrons protocol API v2
 from opentrons import protocol_api
+#### Import math 
+import math ## To do some calculations
 #### For simulating in the app, set pathway to modules
 import sys
 sys.path.append("C:/Program files/Opentrons")
 #### Import mollab protocol module
 from data.user_storage.mollab_modules import Pipetting_Modules as PM
-from data.user_storage.mollab_modules import LabWare as LW                       
+from data.user_storage.mollab_modules import LabWare as LW
 # =============================================================================
 
 # METADATA=====================================================================
@@ -92,13 +94,23 @@ def add_parameters(parameters: protocol_api.Parameters):
                            {"display_name": "H", "value": "H"}
                            ],
                        default="A")
-    parameters.add_int(variable_name="starting_tip_p20_column",
+    parameters.add_str(variable_name="starting_tip_p20_column",    
                        display_name="starting tip p20 column",
-                       description="What column number is the starting tip in?",
-                       default=1,
-                       minimum=1,
-                       maximum=12,
-                       unit="")
+                       choices=[
+                           {"display_name": " 1", "value": " 1"},
+                           {"display_name": " 2", "value": " 2"},
+                           {"display_name": " 3", "value": " 3"},
+                           {"display_name": " 4", "value": " 4"},
+                           {"display_name": " 5", "value": " 5"},
+                           {"display_name": " 6", "value": " 6"},
+                           {"display_name": " 7", "value": " 7"},
+                           {"display_name": " 8", "value": " 8"},
+                           {"display_name": " 9", "value": " 9"},
+                           {"display_name": " 10", "value": " 10"},
+                           {"display_name": " 11", "value": " 11"},
+                           {"display_name": " 12", "value": " 12"}
+                           ],
+                       default=" 1")
     
     parameters.add_str(variable_name="starting_tip_p300_row",    
                        display_name="starting tip p300 row",
@@ -113,34 +125,95 @@ def add_parameters(parameters: protocol_api.Parameters):
                            {"display_name": "H", "value": "H"}
                            ],
                        default="A")
-    parameters.add_int(variable_name="starting_tip_p300_column",
+    parameters.add_str(variable_name="starting_tip_p300_column",    
                        display_name="starting tip p300 column",
-                       description="What column number is the starting tip in?",
-                       default=1,
-                       minimum=1,
-                       maximum=12,
-                       unit="")
-
+                       choices=[
+                           {"display_name": " 1", "value": " 1"},
+                           {"display_name": " 2", "value": " 2"},
+                           {"display_name": " 3", "value": " 3"},
+                           {"display_name": " 4", "value": " 4"},
+                           {"display_name": " 5", "value": " 5"},
+                           {"display_name": " 6", "value": " 6"},
+                           {"display_name": " 7", "value": " 7"},
+                           {"display_name": " 8", "value": " 8"},
+                           {"display_name": " 9", "value": " 9"},
+                           {"display_name": " 10", "value": " 10"},
+                           {"display_name": " 11", "value": " 11"},
+                           {"display_name": " 12", "value": " 12"}
+                           ],
+                       default=" 1")
+    
+    parameters.add_bool(variable_name="lights_on",
+                        display_name="lights on",
+                        description="Do you want the lights turned ON?",
+                        default=False)
+    parameters.add_bool(variable_name="pause",
+                        display_name="pause after mix",
+                        description="Do you want to pause after adding mix, before adding primers?",
+                        default=False)   
+    
 def run(protocol: protocol_api.ProtocolContext):    
 # =============================================================================
 
-# CALCULATED VARIABLES=========================================================
-# =============================================================================
-    strips_per_rack = protocol.params.strips_per_rack
-    print(strips_per_rack)
-
+## CALCULATED VARIABLES========================================================
+## ============================================================================
+    #### Location of primer strips in racks
+    possible_primer_locations = {
+        1:['6'],
+        2:['3','9'],
+        3:['2','7','11'],
+        4:['2','5','8','11'],
+        5:['1', '3', '6','9','12'],
+        6:['1','3','5','7','9','11']
+        }
+    primer_loc = possible_primer_locations[protocol.params.strips_per_rack]    
+    ## How many tube_strip_racks are needed
+    primer_per_rack = 8 * len(primer_loc)
+    number_of_primer_racks = math.ceil(protocol.params.number_of_barcodes / primer_per_rack)
+    
+    #### Starting tips
+    starting_tip_p20_row = protocol.params.starting_tip_p20_row
+    starting_tip_p20_column = protocol.params.starting_tip_p20_column.strip()
+    starting_tip_p20 = starting_tip_p20_row + starting_tip_p20_column
+   
+    starting_tip_p300_row = protocol.params.starting_tip_p300_row
+    starting_tip_p300_column = protocol.params.starting_tip_p300_column.strip()
+    starting_tip_p300 = starting_tip_p300_row + starting_tip_p300_column    
 ## ============================================================================
 
+## COMMENTS====================================================================
+## ============================================================================
 
+## ============================================================================
 
 ## LIGHTS======================================================================
 ## ============================================================================
-    protocol.set_rail_lights(True)
+    if protocol.params.lights_on:
+        protocol.set_rail_lights(True)
+    else:
+        protocol.set_rail_lights(False)
 ## ============================================================================    
 
+# LOADING LABWARE AND PIPETTES=================================================
+# =============================================================================
+    #### Pipette tips
+    tips_mastermix = LW.amount_of_tips(protocol.params.mastermix_volume,
+                                       protocol.params.number_of_barcodes,
+                                       16,
+                                       19)
+    tips_barcodes = LW.amount_of_tips(protocol.params.primer_vol,
+                                       protocol.params.number_of_barcodes * 2,
+                                       1,
+                                       15)
+    amount_tips_20 = tips_mastermix[0] + tips_barcodes[0]
+    amount_tips_300 = tips_mastermix[1] + tips_barcodes[1]
 
-
-
-
-
+    tip_racks_p20 = LW.number_of_tipracks(starting_tip_p20,
+                                          amount_tips_20)
+    tip_racks_p300 = LW.number_of_tipracks(starting_tip_p300,
+                                           amount_tips_300)
+    
+    print(tip_racks_p20)
+    print(tip_racks_p300)
+    
     
